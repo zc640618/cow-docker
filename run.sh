4ec8416b39e763fcfbe8e5d983047154f945e6e8
guolin <mr.guo.lin@gmail.com>
2016-04-13 16:09:04 +0800

    更新了现实内容

--- run.sh
+++ run.sh
@@ -1,12 +1,14 @@
+echo "listen = http://0.0.0.0:$COW_SERVER_PORT \nproxy = ss://$SS_METHOD:$SS_PASSWORD@$SS_SERVER_ADDR:$SS_SERVER_PORT" > rc
 echo "========================================================================"
-echo " You can now connect to this ShadowSocks server:"
+echo " You can now connect to this Cow server:"
 echo ""
 echo " port: $COW_SERVER_PORT"
 echo " ShadowSocks server: ss://$SS_METHOD:$SS_PASSWORD@$SS_SERVER_ADDR:$SS_SERVER_PORT"
-echo " pac url is http://0.0.00:$COW_SERVER_PORT/pac"
+echo " pac url is http://0.0.0.0:$COW_SERVER_PORT/pac"
 echo ""
+echo " config file 'rc':"
+cat rc
 echo " everyone can connect this server, So keep secret!"
 echo "========================================================================"
 
-echo "listen = http://0.0.0.0:$COW_SERVER_PORT \n proxy = ss://$SS_METHOD:$SS_PASSWORD@$SS_SERVER_ADDR:$SS_SERVER_PORT" > rc
 ./cow -rc='rc' 
