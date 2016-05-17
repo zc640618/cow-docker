c08f32ecd1ee4f9ea674da6b0120485f0525406a
zc640618 <zc640618@users.noreply.github.com>
2016-05-17 08:31:42 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ echo "listen = $COW_SERVER_ADDR:$COW_SERVER_PORT $PAC_SERVER_ADDR:$PAC_SERVER_PO
 echo "========================================================================"
 echo " You can now connect to this Cow server:"
 echo ""
-echo " Cow Server: $PAC_SERVER_ADDR:$PAC_SERVER_PORT"
+echo " Cow Server: $COW_SERVER_ADDR:$COW_SERVER_PORT"
 echo " ShadowSocks server: ss://$SS_METHOD:$SS_PASSWORD@$SS_SERVER_ADDR:$SS_SERVER_PORT"
 echo " pac url is http:/$PAC_SERVER_ADDR:$PAC_SERVER_PORT/pac"
 echo ""
