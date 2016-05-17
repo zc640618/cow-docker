29d76bcc601ecdff6168ad3d2339a91f9eda5c0a
zc640618 <zc640618@users.noreply.github.com>
2016-05-17 08:04:27 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
-echo "listen = http://$COW_SERVER_ADDR:$COW_SERVER_PORT \nproxy = ss://$SS_METHOD:$SS_PASSWORD@$SS_SERVER_ADDR:$SS_SERVER_PORT" > rc
+echo "listen = http://127.0.0.1:7777 $PAC_SERVER_ADDR:$PAC_SERVER_PORT \nproxy = ss://$SS_METHOD:$SS_PASSWORD@$SS_SERVER_ADDR:$SS_SERVER_PORT" > rc
 echo "========================================================================"
 echo " You can now connect to this Cow server:"
 echo ""
-echo " Cow Server: $COW_SERVER_ADDR:$COW_SERVER_PORT"
+echo " Cow Server: $PAC_SERVER_ADDR:$PAC_SERVER_PORT"
 echo " ShadowSocks server: ss://$SS_METHOD:$SS_PASSWORD@$SS_SERVER_ADDR:$SS_SERVER_PORT"
-echo " pac url is http://0.0.0.0:$COW_SERVER_PORT/pac"
+echo " pac url is http:/$PAC_SERVER_ADDR:$PAC_SERVER_PORT/pac"
 echo ""
 echo " config file 'rc':"
 cat rc
